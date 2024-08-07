835ff8b4e43291b79fffd03c76398caacb7cffd5
jani <jani.vilkki@gmail.com>
2024-08-07 13:55:49 -0500

    Pokemon Red Fates Nuzlocke

---
+++ test.js
@@ -0,0 +1,152 @@
+class routeEncounters{
+    constructor(route_name, pokemon){
+        this.route_name = route_name;
+        this.pokemon = pokemon;
+    }
+}
+const pallet_town = new routeEncounters("pallet_town", [['bulbasaur', 1], ['charmander', 2], ['squirtle', 3]]);
+const kanto_route_1 = new routeEncounters("kanto_route_1", [['pidgey', 6], ['rattata', 7]]);
+const viridian_city = new routeEncounters("viridian_city", [['poliwag', 26], ['tentacool', 30], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_22 = new routeEncounters("kanto_route_22", [['rattata', 7], ['spearow', 8], ['nidoran-f', 12], ['nidoran-m', 13], ['poliwag', 26], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_2 = new routeEncounters("kanto_route_2", [['weedle', 5], ['pidgey', 6], ['rattata', 7]]);
+const viridian_forest = new routeEncounters("viridian_forest", [['caterpie', 4], ['metapod', 4], ['weedle', 5], ['kakuna', 5], ['pikachu', 10]]);
+const pewter_city = new routeEncounters("pewter_city", []);
+const kanto_route_3 = new routeEncounters("kanto_route_3", [['pidgey', 6], ['spearow', 8], ['jigglypuff', 16], ['magikarp', 64]]);
+const mt_moon = new routeEncounters("mt_moon", [['clefairy', 14], ['zubat', 17], ['paras', 19], ['geodude', 31]]);
+const kanto_route_4 = new routeEncounters("kanto_route_4", [['rattata', 7], ['spearow', 8], ['ekans', 9], ['magikarp', 64]]);
+const cerulean_city = new routeEncounters("cerulean_city", [['psyduck', 23], ['poliwag', 26], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_24 = new routeEncounters("kanto_route_24", [['weedle', 5], ['kakuna', 5], ['pidgey', 6], ['oddish', 18], ['psyduck', 23], ['poliwag', 26], ['abra', 27], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_5 = new routeEncounters("kanto_route_5", [['pidgey', 6], ['oddish', 18], ['mankey', 24]]);
+const kanto_route_6 = new routeEncounters("kanto_route_6", [['pidgey', 6], ['oddish', 18], ['mankey', 24], ['poliwag', 26], ['shellder', 39], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const vermilion_city = new routeEncounters("vermilion_city", [['poliwag', 26], ['shellder', 39], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_11 = new routeEncounters("kanto_route_11", [['spearow', 8], ['ekans', 9], ['poliwag', 26], ['shellder', 39], ['drowzee', 42], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const ss_anne = new routeEncounters("ss_anne", []);
+const kanto_route_9 = new routeEncounters("kanto_route_9", [['rattata', 7], ['spearow', 8], ['ekans', 9]]);
+const kanto_route_10 = new routeEncounters("kanto_route_10", [['spearow', 8], ['ekans', 9], ['poliwag', 26], ['poliwhirl', 26], ['slowpoke', 33], ['voltorb', 44], ['goldeen', 55], ['magikarp', 64]]);
+const rock_tunnel = new routeEncounters("rock_tunnel", [['zubat', 17], ['machop', 28], ['geodude', 31], ['onix', 41]]);
+const lavender_town = new routeEncounters("lavender_town", []);
+const pokemon_tower = new routeEncounters("pokemon_tower", [['gastly', 40], ['haunter', 40], ['cubone', 46]]);
+const kanto_route_8 = new routeEncounters("kanto_route_8", [['pidgey', 6], ['ekans', 9], ['mankey', 24], ['growlithe', 25]]);
+const kanto_route_7 = new routeEncounters("kanto_route_7", [['pidgey', 6], ['oddish', 18], ['mankey', 24], ['growlithe', 25]]);
+const celadon_city = new routeEncounters("celadon_city", [['poliwag', 26], ['poliwhirl', 26], ['slowpoke', 33], ['goldeen', 55], ['magikarp', 64], ['eevee', 67]]);
+const kanto_route_16 = new routeEncounters("kanto_route_16", [['rattata', 7], ['raticate', 7], ['spearow', 8], ['doduo', 36], ['snorlax', 72]]);
+const kanto_route_17 = new routeEncounters("kanto_route_17", [['raticate', 7], ['spearow', 8], ['fearow', 8], ['poliwag', 26], ['tentacool', 30], ['doduo', 36], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const kanto_route_18 = new routeEncounters("kanto_route_18", [['raticate', 7], ['spearow', 8], ['fearow', 8], ['poliwag', 26], ['tentacool', 30], ['doduo', 36], ['krabby', 43], ['goldeen', 55], ['magikarp', 64]]);
+const fuchsia_city = new routeEncounters("fuchsia_city", [['poliwag', 26], ['krabby', 43], ['goldeen', 55], ['seaking', 55], ['magikarp', 64]]);
+const kanto_safari_zone = new routeEncounters("kanto_safari_zone", [['nidorina', 12], ['nidoran-m', 13], ['nidorino', 13], ['parasect', 19], ['venonat', 20], ['psyduck', 23], ['poliwag', 26], ['slowpoke', 33], ['krabby', 43], ['exeggcute', 45], ['rhyhorn', 50], ['chansey', 51], ['goldeen', 55], ['scyther', 58], ['magikarp', 64], ['dratini', 76], ['nidoran-f', 12], ['paras', 19], ['doduo', 36], ['kangaskhan', 53], ['venomoth', 20], ['tauros', 63]]);
+const kanto_route_15 = new routeEncounters("kanto_route_15", [['pidgey', 6], ['pidgeotto', 6], ['oddish', 18], ['gloom', 18], ['venonat', 20], ['ditto', 66]]);
+const kanto_sea_route_19 = new routeEncounters("kanto_sea_route_19", [['poliwag', 26], ['tentacool', 30], ['shellder', 39], ['horsea', 54], ['goldeen', 55], ['staryu', 56], ['magikarp', 64]]);
+const saffron_city = new routeEncounters("saffron_city", [['hitmonlee', 47], ['hitmonchan', 47], ['lapras', 65]]);
+const kanto_sea_route_20 = new routeEncounters("kanto_sea_route_20", [['poliwag', 26], ['tentacool', 30], ['shellder', 39], ['horsea', 54], ['goldeen', 55], ['staryu', 56], ['magikarp', 64]]);
+const kanto_sea_route_21 = new routeEncounters("kanto_sea_route_21", [['pidgey', 6], ['pidgeotto', 6], ['rattata', 7], ['raticate', 7], ['poliwag', 26], ['tentacool', 30], ['shellder', 39], ['tangela', 52], ['horsea', 54], ['goldeen', 55], ['staryu', 56], ['magikarp', 64]]);
+const seafoam_islands = new routeEncounters("seafoam_islands", [['zubat', 17], ['golbat', 17], ['psyduck', 23], ['golduck', 23], ['slowpoke', 33], ['seel', 37], ['shellder', 39], ['horsea', 54], ['dewgong', 37], ['seadra', 54], ['staryu', 56], ['slowbro', 33], ['poliwag', 26], ['goldeen', 55], ['magikarp', 64], ['articuno', 73]]);
+const cinnabar_island = new routeEncounters("cinnabar_island", [['poliwag', 26], ['shellder', 39], ['horsea', 54], ['goldeen', 55], ['staryu', 56], ['magikarp', 64], ['omanyte', 69], ['kabuto', 70], ['aerodactyl', 71]]);
+const pokemon_mansion = new routeEncounters("pokemon_mansion", [['growlithe', 25], ['ponyta', 32], ['grimer', 38], ['muk', 38], ['koffing', 49], ['weezing', 49]]);
+const power_plant = new routeEncounters("power_plant", [['pikachu', 10], ['magnemite', 34], ['magneton', 34], ['voltorb', 44], ['electrode', 44], ['electabuzz', 60], ['zapdos', 74]]);
+
+
+const encounters = [pallet_town, kanto_route_1, viridian_city, kanto_route_22, 
+    kanto_route_2, viridian_forest, pewter_city, kanto_route_3, mt_moon,
+    kanto_route_4, cerulean_city, kanto_route_24, kanto_route_5, kanto_route_6,
+    vermilion_city, kanto_route_11, ss_anne, kanto_route_9, kanto_route_10,
+    rock_tunnel, lavender_town, kanto_route_8, kanto_route_7, celadon_city,
+    pokemon_tower, kanto_route_16, kanto_route_17, kanto_route_18, fuchsia_city,
+    kanto_safari_zone, kanto_route_15, kanto_sea_route_20,
+    kanto_sea_route_19, kanto_sea_route_21, seafoam_islands,
+    cinnabar_island, pokemon_mansion, power_plant]
+
+function generateTable(){
+    const rolled_encounters = rollEncounters();
+    var third = Math.ceil(rolled_encounters.length / 3);
+    var twothird = 2 * third;
+    const chunk_1 = rolled_encounters.slice(0, third);
+    const chunk_2 = rolled_encounters.slice(third, twothird-1);
+    const chunk_3 = rolled_encounters.slice(twothird-1, rolled_encounters.length);
+    const column_1_name = "column1";
+    const column_2_name = "column2";
+    const column_3_name = "column3";
+    const table_1_name = "pokes_table1";
+    const table_2_name = "pokes_table2";
+    const table_3_name = "pokes_table3";
+    makeTable(chunk_1, column_1_name, table_1_name);
+    makeTable(chunk_2, column_2_name, table_2_name);
+    makeTable(chunk_3, column_3_name, table_3_name);
+
+}
+function makeTable(encs, col_name, tbl_name){
+    const parentNode = document.getElementById(col_name);
+    const child_check = document.getElementById(tbl_name);
+    if(parentNode.contains(child_check)) {
+        child_check.remove();
+    }
+    const tbl = document.createElement("table");
+    tbl.id = tbl_name;
+    const tblbody = document.createElement("tbody");
+    for (let i = 0; i<encs.length; i++){
+        const row = document.createElement("tr");
+        var route_name = encs[i][0];
+        var rand = encs[i][1];
+        for(let j=0; j<2; j++){
+            var res_text = "";
+            if(j === 0){
+                res_text = routeNameCleaner(route_name);
+            }
+            else{
+                res_text = rand.charAt(0).toUpperCase() + rand.slice(1);
+            }
+            const cell = document.createElement("td");
+            const cellText = document.createTextNode(res_text);
+            cell.appendChild(cellText);
+            row.appendChild(cell);
+        }
+        tblbody.appendChild(row);
+    }
+    tbl.appendChild(tblbody);
+    parentNode.appendChild(tbl);
+}
+function routeNameCleaner(route){
+     var arr = route.split("_");
+     var res_string = "";
+     for (word of arr){
+        if (word !== "kanto" && word !=="sea"){
+            if (isNaN(word)){
+                var temp = word.charAt(0).toUpperCase() + word.slice(1);
+                res_string += temp + " ";
+            }
+            else{
+                res_string += word + " ";
+            }
+        }
+     }
+     return res_string;
+
+}
+function rollEncounters(){
+    var rolled_encounters = [];
+    var used_pokes = [];
+    for (let i=0; i<encounters.length;i++){
+        var rand = roll(used_pokes, encounters[i]);
+        if(rand[0] !== "None"){
+            used_pokes.push(rand[1]);
+        }
+        rolled_encounters[i] = [encounters[i].route_name, rand[0]];
+    }
+    return rolled_encounters;
+}
+function roll(used_pokes, encs){
+    var unused_pokes = [];
+    var pokes = encs.pokemon;
+    if (pokes.length < 1){
+        return ["None", -1];
+    }
+    for (let i=0; i<pokes.length; i++){
+        if(!(used_pokes.includes(pokes[i][1]))){
+            unused_pokes.push(pokes[i]);
+        }
+    }
+    if(unused_pokes.length < 1){
+        return ["None", -1];
+    }
+    var res = unused_pokes[Math.floor(Math.random() * unused_pokes.length)];
+    return res;
+}
